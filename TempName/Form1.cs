d6ee2af93c94edd9b0263b45e1d569ea98905061
Corpen's Den <ojhitchcock@gmail.com>
2016-04-19 21:45:38 +0100

    Added a border

--- TempName/Form1.cs
+++ TempName/Form1.cs
@@ -17,6 +17,11 @@ namespace TempName
             InitializeComponent();
         }
 
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.FromArgb(21, 133, 181), ButtonBorderStyle.Solid);
+        }
+
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HTCAPTION = 0x2;
         [DllImport("User32.dll")]
